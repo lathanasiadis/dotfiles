4bab0c002acde36e7614966bbfbcdb6aa1a20ca2
Lazaros Athanasiadis <lathanasiadis.dsml@gmail.com>
2025-01-18 16:14:11 +0200

    autorun: remove unused commands

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
 run() {
-  if ! pgrep -f "$1" ;
-  then
-    "$@"&
-  fi
+    if ! pgrep -f "$1" ;
+    then
+        "$@"&
+    fi
 }
 
 setxkbmap -layout us,gr -option 'grp:win_space_toggle' -option 'compose:ralt'
 
 run flameshot
 run picom --backend xrender
-
-#run nm-applet
-
-#run dunst
-
 run redshift -l 37.98:23.73
